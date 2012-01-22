b5267b65b07b3b9fa3a4ecce11c965780d257353
Ubuntu <ubuntu@domU-12-31-39-14-F2-F6.compute-1.internal>
2012-01-22 00:29:49 +0000

    reset

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Qrserve::Application.config.secret_token = '024d2f6a34d9148f537e11550beac44595d920f72043efd9434b411fb49afa984f0cffc9e4200f6758d0eb715c5e5b42ea29c8ff7d57d4683b7f77b67b24a2c6'
